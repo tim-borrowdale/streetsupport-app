4eea554c0b639715d4696973f18096fd8d7d2bab
Tony McBeth <tony.mcbeth@gmail.com>
2016-08-15 06:25:32 +0100

    updated org detail template

--- src/app/pages/organisation/organisation.js
+++ src/app/pages/organisation/organisation.js
@@ -1,10 +1,10 @@
 import {Component} from '@angular/core';
-import {NavController, NavParams, Loading} from 'ionic-angular';
+import {NavController, NavParams, Loading, ActionSheet} from 'ionic-angular';
 import {ContentService} from '../../services/content-service';
 
 
 @Component({
-  templateUrl: 'build/pages/organisation/organisation.html',
+  templateUrl: 'build/pages/organisation/organisation.html'
 })
 export class OrganisationPage {
   static get parameters() {
@@ -37,5 +37,54 @@ export class OrganisationPage {
     });
 
     this.nav.present(this.loading);
+  }
+
+  presentActionSheet() {
+    let actionSheet = ActionSheet.create({
+      title: 'Contact Organisation',
+      buttons: [
+        {
+          text: 'Telephone',
+          handler: () => {
+            window.location = `tel:${this.organisation.telephone}`;
+          }
+        },{
+          text: 'Email',
+          handler: () => {
+
+          }
+        },{
+          text: 'Website',
+          handler: () => {
+            cordova.InAppBrowser.open(this.organisation.website, "_blank", "location=no,toolbarposition=top");
+          }
+        },{
+          text: 'Facebook',
+          handler: () => {
+            cordova.InAppBrowser.open(this.organisation.facebook, "_blank", "location=no,toolbarposition=top");
+          }
+        },{
+          text: 'Twitter',
+          handler: () => {
+            cordova.InAppBrowser.open(this.organisation.twitter, "_blank", "location=no,toolbarposition=top");
+          }
+        },{
+          text: 'Cancel',
+          role: 'cancel'
+        }
+      ]
+    });
+    this.nav.present(actionSheet);
+  }
+
+  sortAlphabetically(collection) {
+
+    return collection.sort(function(a, b) {
+      return a.name.localeCompare(b.name);
+    });
+  }
+
+  itemTapped(event, service) {
+
   }
 }
