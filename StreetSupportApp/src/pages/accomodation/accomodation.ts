4e608ccc949895768787af532cda7fe2b077534f
Tony McBeth <tony.mcbeth@gmail.com>
2017-10-10 22:34:21 +0100

    updated accomodation copy

--- StreetSupportApp/src/pages/accomodation/accomodation.ts
+++ StreetSupportApp/src/pages/accomodation/accomodation.ts
@@ -1,8 +1,6 @@
 import {Component} from '@angular/core';
 import {NavController, NavParams} from 'ionic-angular';
 import { InAppBrowser } from '@ionic-native/in-app-browser';
-import {STRINGS} from "../../constants";
-
 
 @Component({
   selector: 'accomodation',
@@ -10,14 +8,12 @@ import {STRINGS} from "../../constants";
 })
 export class AccomodationPage {
 
-  public content: string;
-
   constructor(public nav: NavController, navParams: NavParams, private iab: InAppBrowser) {
-    this.content = STRINGS['ACCOMODATION_COPY'];
+
   }
 
-  presentWebPage() {
-    let browser = this.iab.create('https://streetsupport.net/find-help/accommodation/', '_system','');
+  itemTapped(url: string) {
+    let browser = this.iab.create(url, '_system','');
     browser.show();
   }
 }
