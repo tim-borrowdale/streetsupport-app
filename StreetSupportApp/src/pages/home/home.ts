98b542995e006f9ef282057ff06fc42ddb3c4544
Vince Lee <vslee888@gmail.com>
2018-11-02 17:10:25 +0000

    location specific nav items

--- StreetSupportApp/src/pages/home/home.ts
+++ StreetSupportApp/src/pages/home/home.ts
@@ -1,19 +1,36 @@
-import {Component} from '@angular/core';
-import {InAppBrowser} from '@ionic-native/in-app-browser';
-import {NavController} from 'ionic-angular';
-import {EmergencyPage} from '../emergency/emergency';
-
+import { Component } from '@angular/core';
+import { InAppBrowser } from '@ionic-native/in-app-browser';
+import { NavController } from 'ionic-angular';
+import { EmergencyPage } from '../emergency/emergency';
+import { LocationProvider } from '../../providers/location-provider';
 
 @Component({
   selector: 'home',
   templateUrl: 'home.html',
 })
 export class HomePage {
+  public currentLocation = {}
 
-  constructor(public nav: NavController, private iab: InAppBrowser) { }
+  constructor(
+    public nav: NavController,
+    public locationProvider: LocationProvider,
+    private iab: InAppBrowser) {
+
+  }
+
+  ionViewWillEnter() {
+    this.locationProvider.getCurrentCity()
+      .then((city) => {
+        this.currentLocation = city;
+      })
+
+    this.locationProvider.newLocation.subscribe((newLocation) => {
+        this.currentLocation = newLocation;
+      })
+  }
 
   launch(url) {
-    const browser = this.iab.create(url, '_system');
+    this.iab.create(url, '_system');
   }
 
   emergencyItemTapped() {
