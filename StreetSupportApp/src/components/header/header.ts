1e178ad213e4bec82e80771228c24f41fd1e6060
Vince Lee <vince.lee@polyhatsoftware.co.uk>
2019-02-08 10:30:49 +0000

    removed redundant imports

--- StreetSupportApp/src/components/header/header.ts
+++ StreetSupportApp/src/components/header/header.ts
@@ -1,4 +1,4 @@
-import { Component, Output, EventEmitter } from '@angular/core';
+import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { LocationProvider } from '../../providers/location-provider';
 import { SettingsPage } from '../../pages/settings/settings';
